e99459fc583c09df9255a29f0db99d3074a8f8f8
MnpretDevs07 <manpreet.31oct2002@gmail.com>
2024-08-02 22:32:02 +0530

    Initialize project using Create React App

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <h1>Let's Create & Complete Goals</h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
